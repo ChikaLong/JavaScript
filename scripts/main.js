4b9d1d5bc58a04a48fe67a50077804da10a78405
ChikaLong <longb.0818@gmail.com>
2022-02-27 21:11:50 +0900

    jsファイルの整理

--- scripts/main.js
+++ scripts/main.js
@@ -1,37 +1,78 @@
 document.addEventListener('DOMContentLoaded', function () {
-  const hero = new HeroSlider('.swiper-container');
-  hero.start();
+  const main = new Main();
+});
 
-  const cb = function (el, inview) {
+class Main{
+  constructor(){
+    this.header = document.querySelector('.header');
+    this._observers = [];
+    this._init();
+  }
+
+  set observers(val){
+    this._observers.push(val);
+  }
+
+  get observers(){
+    return this._observers;
+  }
+
+  _init(){
+    new MobileMenu();
+    this.hero = new HeroSlider('.swiper-container');
+    Pace.once('done', this._paceDone.bind(this));
+  }
+
+  _paceDone(){
+    this._scrollInit();
+  }
+
+  _textAnimation(el, inview) {
     if(inview) {
       const ta = new TweenTextAnimation(el);
       ta.animate();
     }
   }
-  const so = new ScrollObserver('.tween-animate-title', cb);
 
-
-  const _inviewAnimation = function(el, inview) {
+  _inviewAnimation(el, inview) {
     if(inview){
       el.classList.add('inview');
     } else {
       el.classList.remove('inview');
     }
   }
-  const so2 = new ScrollObserver('.cover-slide', _inviewAnimation);
 
-
-  const header = document.querySelector('.header');
-  const _navAnimation = function(el, inview) {
+  _navAnimation(el, inview) {
     if(inview){
-      header.classList.remove('triggered');
+      this.hero.start();
     } else {
-      header.classList.add('triggered');
+      this.hero.stop();
     }
   }
-  const so3 = new ScrollObserver('.nav-trigger', _navAnimation, {once: false});
+
+  _toggleSlideAnimation(el, inview) {
+    if(inview){
+      this.header.classList.remove('triggered');
+    } else {
+      this.header.classList.add('triggered');
+    }
+  }
+
+  _destroyObservers(){
+    this.observers.forEach(ob => {
+      ob.destroy();
+    });
+  }
+
+  destroy(){
+    this._destroyObservers();
+  }
 
 
-  new MobileMenu();
-});
-
+  _scrollInit(){
+    this.observers = new ScrollObserver('.nav-trigger', this._navAnimation.bind(this), {once: false})
+    this.observers = new ScrollObserver('.cover-slide', this._inviewAnimation)
+    this.observers = new ScrollObserver('.tween-animate-title', this._textAnimation)
+    this.observers = new ScrollObserver('.swiper-container', this._toggleSlideAnimation.bond(this), {once: false})
+  }
+}
